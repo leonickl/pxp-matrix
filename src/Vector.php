af29040b517c946374638e8bef679ac88d4fbd56
Leo Nickl <leol@leonickl.de>
2025-12-12 22:56:04 +0100

    add vector

---
+++ src/Vector.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace PXP\Matrix;
+
+use Exception;
+
+readonly class Vector extends Matrix
+{
+    public function __construct(private array $vector, bool $check = true)
+    {
+        parent::__construct(array_map(fn($entry) => [$entry], $vector), $check);
+
+        if($this->width() !== 1) {
+            throw new Exception('Vector must have width of exactly 1');
+        }
+    }
+
+    public function inner(Vector $other): Matrix
+    {
+        return $this->t()->times($other);
+    }
+
+    public function outer(Vector $other): Matrix
+    {
+        return $this->times($other->t());
+    }
+}
