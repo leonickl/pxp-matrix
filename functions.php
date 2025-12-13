97c54a1de30100b1f4ba4ddb07a8bbb56a3f9fcf
Leo Nickl <leol@leonickl.de>
2025-12-13 19:56:09 +0100

    add check to vector helper

--- functions.php
+++ functions.php
@@ -8,7 +8,7 @@ function matrix(array $matrix, bool $check = true)
     return new Matrix($matrix, check: $check);
 }
 
-function vector(array $vector)
+function vector(array $vector, bool $check = true)
 {
-    return new Vector($vector);
+    return new Vector($vector, check: $check);
 }
